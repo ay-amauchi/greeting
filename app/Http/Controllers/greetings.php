2eb0d01db87e149466a1b0cda1cd32707b2d91c1
ay-amauchi <shanneicai0124@gmail.com>
2022-10-13 20:16:04 +0900

    update some parts

--- app/Http/Controllers/greetings.php
+++ app/Http/Controllers/greetings.php
@@ -6,43 +6,37 @@ use Illuminate\Http\Request;
 
 class greetings extends Controller
 {
-    public function morning()
+    public function greeting($element)
     {
-        $time = '朝';
-        $greeting = 'おはようございます';
-        return view('messages.greeting', ['time' => $time, 'greeting' => $greeting]);
-    }
-
-    public function afternoon()
-    {
-        $time = '昼';
-        $greeting = 'こんにちは';
-        return view('messages.greeting', ['time' => $time, 'greeting' => $greeting]);
-    }
-    public function evening()
-    {
-        $time = '夕方';
-        $greeting = 'こんばんは';
-        return view('messages.greeting', ['time' => $time, 'greeting' => $greeting]);
-    }
-    public function night()
-    {
-        $time = '夜';
-        $greeting = 'おやすみ';
-        return view('messages.greeting', ['time' => $time, 'greeting' => $greeting]);
+        switch ($element) {
+            case 'morning':
+                $title = '朝のあいさつ';
+                $comment = 'おはようございます';
+                break;
+            case 'afternoon':
+                $title = '昼のあいさつ';
+                $comment = 'こんにちは';
+                break;
+            case 'evening':
+                $title = '夕方のあいさつ';
+                $comment = 'こんばんは';
+                break;
+            case 'night':
+                $title = '夜のあいさつ';
+                $comment = 'おやすみ';
+                break;
+            case 'random';
+                $title = 'ランダムなメッセージ';
+                $input = array("おはよう", "こんにちは", "こんばんは", "おやすみ");
+                $rand_key = array_rand($input, 1);
+                $comment = $input[$rand_key];
+                break;
+        }
+        return view('messages.greeting', ['title' => $title, 'comment' => $comment]);
     }
 
     public function freeword($msg)
     {
         return view('messages.freeword', ['msg' => $msg]);
-    }
-
-    public function random()
-    {
-        $input = array("おはよう", "こんにちは", "こんばんは", "おやすみ");
-        $rand_key = array_rand($input, 1);
-        $msg = $input[$rand_key];
-        
-        return view('messages.random', ['msg' => $msg]);
     }
 }
